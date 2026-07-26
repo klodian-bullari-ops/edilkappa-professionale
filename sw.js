f47985bb19f35879d37c49df7c774bafdea466c3
klodian-bullari-ops <klodian-bullari@live.it>
2026-07-26 20:16:23 +0200

    Aggiunge foto e video a preventivi e drone

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "edilkappa-professionale-";
-const CACHE = `${CACHE_PREFIX}v14-preventivi-originali`;
+const CACHE = `${CACHE_PREFIX}v15-foto-video`;
 const APP_SHELL = [
   "./",
   "./index.html",
