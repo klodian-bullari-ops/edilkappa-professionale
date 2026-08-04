197c3efee783207f2d40a1a7fb074ee85af3c392
klodian-bullari-ops <klodian-bullari@live.it>
2026-08-04 19:33:19 +0200

    Aggiorna la cache della galleria foto

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "edilkappa-professionale-";
-const CACHE = `${CACHE_PREFIX}v28-foto-storiche-cantiere`;
+const CACHE = `${CACHE_PREFIX}v29-apertura-galleria-foto`;
 const APP_SHELL = [
   "./",
   "./index.html",
