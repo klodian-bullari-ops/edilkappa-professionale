56561946a3f209226c58f87a007bb2a1041a396c
klodian-bullari-ops <klodian-bullari@live.it>
2026-08-10 00:00:16 +0200

    Aggiorna la cache del routing AI

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "edilkappa-professionale-";
-const CACHE = `${CACHE_PREFIX}v58-foto-pdf`;
+const CACHE = `${CACHE_PREFIX}v59-routing-ai`;
 const APP_SHELL = [
   "./",
   "./index.html",
