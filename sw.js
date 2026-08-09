32d11fa83fdc3fbe110489339b3fb1b25a638ecf
klodian-bullari-ops <klodian-bullari@live.it>
2026-08-09 20:42:26 +0200

    Corregge caricamento foto HEIC

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "edilkappa-professionale-";
-const CACHE = `${CACHE_PREFIX}v55-pdf-professionale`;
+const CACHE = `${CACHE_PREFIX}v56-heic-foto`;
 const APP_SHELL = [
   "./",
   "./index.html",
